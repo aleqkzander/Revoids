532ea9cc16081361d67ec80d1cd39f5f65832830
Alexander Lairich <alexanderlairich@web.de>
2023-01-24 22:33:10 +0100

    v1
    Saving and consent is working correctly now.

---
+++ Revoids/Assets/Created Content/Game Manager/PlayerSettings.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.UI;
+
+public class PlayerSettings : MonoBehaviour
+{
+    [Header("Player Preference Keys")]
+    public string prefsAccepted = "playernotice";
+    public string prefsScore = "playerscore";
+    public string prefsSound = "playersound";
+
+    [Header("Current Settings")]
+    public string playerAccepted;
+    public int playerScore = 0;
+    public string playerSound;
+
+    [Header("Scene References")]
+    public GameObject privacyManager;
+    public GameObject audioManager;
+    public Image audioButton;
+    public List<Sprite> audioButtonImages;
+
+    private void Start()
+    {
+        LoadGameState();
+    }
+
+
+    /// <summary>
+    /// load gamestate on start
+    /// </summary>
+    public void LoadGameState()
+    {
+        // load current settings
+        LoadSettings();
+
+        // show privacy or not
+        if (playerAccepted == "accepted") privacyManager.SetActive(false);
+
+        // play sound or not
+        if (playerSound == "enabled")
+        {
+            audioManager.SetActive(true);
+            audioButton.sprite = audioButtonImages[0];
+        }
+        else
+        {
+            audioManager.SetActive(false);
+            audioButton.sprite = audioButtonImages[1];
+        }
+    }
+
+
+    /// <summary>
+    /// use method for saving
+    /// </summary>
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetString(prefsAccepted, playerAccepted);
+        PlayerPrefs.SetInt(prefsScore, playerScore);
+        PlayerPrefs.SetString(prefsSound, playerSound);
+        PlayerPrefs.Save();
+    }
+
+
+    /// <summary>
+    /// use method for loading
+    /// </summary>
+    public void LoadSettings()
+    {
+        playerAccepted = PlayerPrefs.GetString(prefsAccepted);
+        playerScore = PlayerPrefs.GetInt(prefsScore);
+        playerSound = PlayerPrefs.GetString(prefsSound);
+    }
+
+
+    /// <summary>
+    /// use method to accept privacy
+    /// </summary>
+    public void AcceptPrivacy()
+    {
+        // if the user opts in to targeted advertising:
+        MetaData gdprMetaData = new MetaData("gdpr");
+        gdprMetaData.Set("consent", "true");
+        Advertisement.SetMetaData(gdprMetaData);
+
+        // set and save
+        playerAccepted = "accepted";
+        SaveSettings();
+    }
+
+
+    /// <summary>
+    /// use method to revoke privacy
+    /// </summary>
+    public void RevokePrivacy()
+    {
+        // if the user opts out of targeted advertising:
+        MetaData gdprMetaData = new MetaData("gdpr");
+        gdprMetaData.Set("consent", "false");
+        Advertisement.SetMetaData(gdprMetaData);
+
+        // delete data
+        PlayerPrefs.DeleteAll();
+
+        // quit
+        Application.Quit();
+    }
+
+
+    /// <summary>
+    /// use method to activate/deactivate audio
+    /// </summary>
+    public void SwitchAudio()
+    {
+        if (audioManager.activeSelf)
+        {
+            audioManager.SetActive(false);
+            playerSound = "";
+            audioButton.sprite = audioButtonImages[1];
+            SaveSettings();
+        }
+        else
+        {
+            audioManager.SetActive(true);
+            playerSound = "enabled";
+            audioButton.sprite = audioButtonImages[0];
+            SaveSettings();
+        }
+    }
+}
--- Revoids/Assets/Created Content/Privacy/PrivacySettings.cs
+++ Revoids/Assets/Created Content/Privacy/PrivacySettings.cs
@@ -1,78 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Advertisements;
-
-public enum PrivacyState
-{
-    accpeted,
-    notaccepted
-}
-
 
 public class PrivacySettings : MonoBehaviour
 {
-    public PlayerSaveFile playerPrefs;
-
-    private void Start()
-    {
-        CheckStatus();
-    }
+    public PlayerSettings settings;
 
 
     /// <summary>
-    /// check the current status in playerprefs
+    /// method to accept privacy notice
     /// </summary>
-    public void CheckStatus()
+    public void Button_AcceptAndContinue()
     {
-        if (playerPrefs.privacyNotice == PrivacyState.accpeted.ToString())
-        {
-            gameObject.SetActive(false);
-        }
-    }
-
-
-    /// <summary>
-    ///  set state in playerprefs
-    /// </summary>
-    public void Accept() 
-    {
-        // if the user opts in to targeted advertising:
-        MetaData gdprMetaData = new MetaData("gdpr");
-        gdprMetaData.Set("consent", "true");
-        Advertisement.SetMetaData(gdprMetaData);
-
-        // save internally
-        playerPrefs.SetPrivacyNotice(PrivacyState.accpeted.ToString());
-        playerPrefs.privacyNotice = PrivacyState.accpeted.ToString();
-        CheckStatus();
+        settings.AcceptPrivacy();
+        gameObject.SetActive(false);
     }
 
 
     /// <summary>
     /// open link for more information
     /// </summary>
-    /// <param name="link"></param>
-    public void OpenLink(string link)
+    /// <param name="url"></param>
+    public void Button_MoreInformation(string url)
     {
-        Application.OpenURL(link);
+        Application.OpenURL(url);
     }
 
 
     /// <summary>
-    ///  use to opt-out and quit the game
+    /// revoke access
     /// </summary>
-    public void OptOut()
+    public void Button_DeleteAndQuit()
     {
-        // if the user opts out of targeted advertising:
-        MetaData gdprMetaData = new MetaData("gdpr");
-        gdprMetaData.Set("consent", "false");
-        Advertisement.SetMetaData(gdprMetaData);
-
-        // delete data
-        PlayerPrefs.DeleteAll();
-
-        // quit
-        Application.Quit();
+        settings.RevokePrivacy();
     }
+
 }
