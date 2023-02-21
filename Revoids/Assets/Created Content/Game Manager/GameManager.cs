1869bac4639915db780777c7502cd618c7b31cbe
Alexander Lairich <alexanderlairich@web.de>
2023-02-21 06:39:45 +0100

    v4
    Adjust the background so the game is not too dark overall.
    
    Displaying the build version now.

--- Revoids/Assets/Created Content/Game Manager/GameManager.cs
+++ Revoids/Assets/Created Content/Game Manager/GameManager.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+
+    public TMP_Text versionText;
+
     private void Start()
     {
         // Make the game run as fast as possible
         // Application.targetFrameRate = 300;
+
+        versionText.text = "VERSION: " + Application.version;
     }
 
     public void ExitGame()
